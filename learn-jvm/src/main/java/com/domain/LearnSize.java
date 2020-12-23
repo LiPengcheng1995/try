d49266df5caf2f0323c732140ac20ffcf88911bc
lipengcheng3 <lipengcheng3@jd.com>
2020-12-23 18:31:45 +0800

    save

---
+++ learn-jvm/src/main/java/com/domain/LearnSize.java
@@ -0,0 +1,17 @@
+package com.domain;
+
+import java.util.HashMap;
+
+/**
+ * @author: 李鹏程
+ * @email: lipengcheng3@jd.com
+ * @date: 2020/12/23
+ * @Time: 14:32
+ * @Description:
+ */
+public class LearnSize {
+    public static void main(String[] args) {
+        HashMap x = new HashMap();
+        System.out.println();
+    }
+}

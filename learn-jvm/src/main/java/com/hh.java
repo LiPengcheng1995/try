093ada501609134c5669a395bb9d17c7c22dfe3b
lipengcheng3 <lipengcheng3@jd.com>
2021-03-10 16:33:15 +0800

    modify:流程图

---
+++ learn-jvm/src/main/java/com/hh.java
@@ -0,0 +1,18 @@
+package com;
+
+/**
+ * @author: 李鹏程
+ * @email: lipengcheng3@jd.com
+ * @date: 2021/3/3
+ * @Time: 17:27
+ * @Description:
+ */
+public class hh {
+    public static void main(String[] args) {
+        String a = "aa";
+        String b = "aax";
+        String c = "x";
+        String d = a+c;
+        System.out.println(d==b);
+    }
+}
